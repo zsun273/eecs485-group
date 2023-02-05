2bc8cc4ec4849cbc127f189fc1fdf8ad08d3a377
hongyiy <hongyiy@umich.edu>
2023-02-05 16:59:35 -0500

    comments rest api

--- p3-insta485-clientside/insta485/api/__init__.py
+++ p3-insta485-clientside/insta485/api/__init__.py
@@ -5,3 +5,5 @@ from insta485.api.posts import get_post
 from insta485.api.index import get_resources
 from insta485.api.likes import create_like
 from insta485.api.likes import delete_like
+from insta485.api.comments import create_comment
+from insta485.api.comments import delete_comment
---
+++ p3-insta485-clientside/insta485/api/comments.py
@@ -0,0 +1,83 @@
+"""Comment Rest API."""
+import flask
+import insta485
+from insta485 import invalid_usage
+
+
+@insta485.app.route('/api/v1/comments/', methods=['POST'])
+def create_comment():
+    """Create a Comment."""
+    username = insta485.model.check_authorization()
+    postid = flask.request.args.get('postid', None)
+    text = flask.request.json.get('text', None)
+
+    # query database
+    post = insta485.model.query_db(
+        "SELECT * "
+        "FROM posts "
+        "WHERE postid = ?",
+        (postid,),
+        one=True
+    )
+
+    # check if the postid exists
+    if not post:
+        raise invalid_usage.InvalidUsage('Not Found', status_code=404)
+
+    # update database
+    if not text:
+        raise invalid_usage.InvalidUsage('Bad Request', status_code=400)
+    insta485.model.update_db(
+        "INSERT INTO comments(owner,postid,text) "
+        "VALUES (?, ?, ?) ",
+        (username, postid, text,)
+    )
+
+    # query database
+    commentid = insta485.model.query_db(
+        "SELECT last_insert_rowid() "
+        "FROM comments "
+    )
+
+    context = {
+        "commentid": commentid,
+        "lognameOwnsThis": (username == post['owner']),
+        "owner": username,
+        "ownerShowUrl": f"/users/{username}/",
+        "text": text,
+        "url": f"/api/v1/comments/{commentid}/"
+    }
+    return flask.jsonify(**context), 201
+
+
+@insta485.app.route('/api/v1/comments/<int:commentid>/', methods=['DELETE'])
+def delete_comment(commentid):
+    """Delete a Comment."""
+    username = insta485.model.check_authorization()
+
+    # query database
+    ans = insta485.model.query_db(
+        "SELECT * "
+        "FROM comments "
+        "WHERE commentid = ?",
+        (commentid,),
+        one=True
+    )
+
+    # check if comment exist
+    if not ans:
+        raise invalid_usage.InvalidUsage('Not Found', status_code=404)
+
+    # check if the logname owns the comment
+    if username != ans['owner']:
+        raise invalid_usage.InvalidUsage('Forbidden', status_code=403)
+
+    # update database
+    insta485.model.update_db(
+        "DELETE FROM comments "
+        "WHERE commentid = ? ",
+        (commentid,)
+    )
+
+    # return success 204 and no content
+    return flask.jsonify({}), 204
